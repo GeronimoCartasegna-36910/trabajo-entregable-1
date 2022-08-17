9d42da12347e9cba6eb297a0f4929f6697cefd19
geronimocartasegna <gerocartasegna10@hotmail.com>
2022-08-17 11:47:10 -0300

    fetch y ajax entregable

--- scrips.js
+++ scrips.js
@@ -6,12 +6,12 @@
  .then(productos =>{
     productos.forEach((producto, indice) => {
         idProductos.innerHTML += `
-        <div id="${indice}" class="card" style="width: 18rem;">
-            <img src="./imagenes/${producto.img} " class="card-img-top">
+        <div id="${indice}" class="card m-3 p-3" style="width: 18rem;">
+            <img src="./imagenes/${producto.img} " class="card-img-top card_img">
                 <div class="card-body">
                     <h2 class="card-title">${producto.nombre}</h2>
-                    <p class="card-text">${producto.precio}</p>
-                    <p class="card-text">${producto.stock}</p>
+                    <p class="card-text">Precio: $${producto.Precio}</p>
+                    <p class="card-text">Stock: ${producto.Stock}</p>
                     <a href="#" class="btn btn-primary">comprar!</a>
                 </div>
         </div>
