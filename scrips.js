d307c989edc1695ed0ce1e07dc45f0317ea62727
geronimocartasegna <gerocartasegna10@hotmail.com>
2022-08-16 23:50:59 -0300

    entregable fetch

--- scrips.js
+++ scrips.js
@@ -1,44 +1,21 @@
 
-class Auto {
-    constructor(marca, modelo, precio, stock){
-        this.marca = marca;
-        this.modelo = modelo;
-        this.precio = precio;
-        this.stock = stock;
-    }
-}
+ const idProductos = document.getElementById("idProductos")
 
-const autos = []
-
-const form = document.getElementById("idForm")
-const divListado = document.getElementById("divListado")
-const btnListado = document.getElementById("btnListado")
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault()
-
-    let marca = document.getElementById("idMarca").value;
-    let modelo = document.getElementById("idModelo").value;
-    let precio = document.getElementById("idPrecio").value;
-    let stock = document.getElementById("idStock").value;
-
-    const auto = new Auto (marca, modelo, precio, stock)
-    autos.push(auto)
-
-    form.reset()
-})
-
-btnListado.addEventListener("click", () => {
-    autos.forEach(auto => {
-        divListado.innerHTML += `
-        <div class="card mt-3" style="width: 18rem;">
-            <div class="card-body">
-                <h2 class="card-title">${auto.modelo}</h2>
-                <p class="card-text">Marca: ${auto.marca}</p>
-                <p class="card-text">Precio: ${auto.precio}</p>
-                <p class="card-text">Stock: ${auto.stock}</p>
-            </div>
+ fetch("./productos.json")
+ .then(response => response.json())
+ .then(productos =>{
+    productos.forEach((producto, indice) => {
+        idProductos.innerHTML += `
+        <div id="${indice}" class="card" style="width: 18rem;">
+            <img src="./imagenes/${producto.img} " class="card-img-top">
+                <div class="card-body">
+                    <h2 class="card-title">${producto.nombre}</h2>
+                    <p class="card-text">${producto.precio}</p>
+                    <p class="card-text">${producto.stock}</p>
+                    <a href="#" class="btn btn-primary">comprar!</a>
+                </div>
         </div>
+        
         `
-    })
-})
+    });
+ })
