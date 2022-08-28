6be828f75655b5fdd76399bca308d670664f80e0
geronimocartasegna <gerocartasegna10@hotmail.com>
2022-08-28 09:42:12 -0300

    trabajo final

--- scrips.js
+++ scrips.js
@@ -1,21 +1,78 @@
+//asigno las variables
+const idProductos = document.getElementById("idProductos")
+const idCarrito = document.getElementById("cart-modal")
+let carrito = []
 
- const idProductos = document.getElementById("idProductos")
+//creo el localstorage
+if(localStorage.getItem("carrito")){
+    carrito = JSON.parse(localStorage.getItem("carrito"))
+} else{
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+}
 
- fetch("./productos.json")
- .then(response => response.json())
- .then(productos =>{
+
+//cargo datos de los productos
+fetch("productos.json")
+.then(response => response.json())
+.then(productos => {
     productos.forEach((producto, indice) => {
         idProductos.innerHTML += `
-        <div id="${indice}" class="card m-3 p-3" style="width: 18rem;">
-            <img src="./imagenes/${producto.img} " class="card-img-top card_img">
-                <div class="card-body">
-                    <h2 class="card-title">${producto.nombre}</h2>
-                    <p class="card-text">Precio: $${producto.Precio}</p>
-                    <p class="card-text">Stock: ${producto.Stock}</p>
-                    <a href="#" class="btn btn-primary">comprar!</a>
-                </div>
-        </div>
         
+        <div class="card" id="producto${indice}" style="width: 18rem;">
+            <img src="./imagenes/${producto.img}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${producto.nombre}</h5>
+                <p class="card-text">Talle: ${producto.talle}</p>
+                <p class="card-text">Precio: $${producto.Precio}</p>
+                <p class="card-text">Stock: ${producto.Stock}</p>
+                <buttom class="btn btn-info">Agregar al carrito</buttom>
+            </div>
+        </div>
         `
-    });
- })
+    })
+
+ //agrego el evento al boton de agregar al carrito
+    productos.forEach((producto, indice) =>{
+        const botonAgregarCarrito = (document.getElementById(`producto${indice}`).lastElementChild.lastElementChild)
+
+        botonAgregarCarrito.addEventListener("click",() => {
+            Toastify({
+                text: "Producto Agregado Correctamente",
+                duration: 2000
+                }).showToast();
+
+             carrito.push(producto)
+             console.log(carrito)
+             localStorage.setItem("carrito", JSON.stringify(carrito))
+             
+             
+        })
+    })
+
+    idCarrito.addEventListener("click", ()=>{
+        
+        Swal.fire({
+            title: 'Tu Carrito',
+        
+            showCancelButton: true,
+            confirmButtonText: 'Finalizar compra',
+            cancelButtonText:"seguir comprando"
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire('Su compra fue realizada con exito', '', 'success')
+            }
+          })
+    })
+
+
+    
+    
+})
+
+
+
+
+    
+
+
+
